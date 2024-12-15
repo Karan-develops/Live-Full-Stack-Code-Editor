d8ff9ba592d1439ca8198f5907cefc765867497f
Karan <mrkaran2k5@gmail.com>
2024-12-15 15:44:10 +0530

    Comments Functionality

--- convex/snippets.ts
+++ convex/snippets.ts
@@ -141,3 +141,79 @@ export const starSnippet = mutation({
     }
   },
 });
+
+export const getSnippetByID = query({
+  args: {
+    snippetId: v.id("snippets"),
+  },
+  handler: async (ctx, args) => {
+    const snippet = await ctx.db.get(args.snippetId);
+    if (!snippet) {
+      throw new Error("No Snippet Found.");
+    }
+    return snippet;
+  },
+});
+
+export const getComments = query({
+  args: {
+    snippetId: v.id("snippets"),
+  },
+  handler: async (ctx, args) => {
+    const comments = await ctx.db
+      .query("snippetComments")
+      .withIndex("by_snippet_id")
+      .filter((q) => q.eq(q.field("snippetId"), args.snippetId))
+      .order("desc")
+      .collect();
+
+    return comments;
+  },
+});
+
+export const addComment = mutation({
+  args: {
+    snippetId: v.id("snippets"),
+    content: v.string(),
+  },
+  handler: async (ctx, args) => {
+    const userIdentity = await ctx.auth.getUserIdentity();
+    if (!userIdentity) {
+      throw new Error("User not authenticated");
+    }
+    const user = await ctx.db
+      .query("users")
+      .withIndex("by_user_id")
+      .filter((q) => q.eq(q.field("userId"), userIdentity.subject))
+      .first();
+    if (!user) {
+      throw new Error("User not found");
+    }
+    return await ctx.db.insert("snippetComments", {
+      snippetId: args.snippetId,
+      userId: userIdentity.subject,
+      userName: user.name,
+      content: args.content,
+    });
+  },
+});
+
+export const deleteComment = mutation({
+  args: {
+    commentId: v.id("snippetComments"),
+  },
+  handler: async (ctx, args) => {
+    const userIdentity = await ctx.auth.getUserIdentity();
+    if (!userIdentity) {
+      throw new Error("User not authenticated");
+    }
+    const comment = await ctx.db.get(args.commentId);
+    if (!comment) {
+      throw new Error("No Comment found");
+    }
+    if (comment.userId !== userIdentity.subject) {
+      throw new Error("UnAuthorized to delete the comment");
+    }
+    await ctx.db.delete(args.commentId);
+  },
+});
